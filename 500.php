1d622618a96e755cc5fbe30c70323b988245967e
AblePixel <ablepixel@gmail.com>
2016-06-28 21:51:34 -0400

    Added Error php template, modified 500.php to reflect this change.

--- 500.php
+++ 500.php
@@ -14,7 +14,7 @@
         if ((include 'balefire/php/errors.php') == FALSE) { //Theoretically, this should never fail. If it does, something is _REALLY_ broken.
             echo 'GENERIC FATAL: BaleFire was unable to run, and we were unable to determine why.<br>Please contact your System Administrator for assistance.<br><br><br>[FATAL: ERROR_NOFIND_ERROR_HANDLER]';
         } else {
-            echo '';
+            include 'balefire/php/errors.php';
         }
         ?>
         </div>
---
+++ balefire/php/errors.php
@@ -0,0 +1,11 @@
+<?php
+/* Balefire Error-Handling, for the error page. Will explain the error, and possible steps that can be taken to resolve it. Separated into Fatal and Non-Fatal, where Fatal errors will force the '500.php' page to be loaded, Non-Fatal errors will not.
+ */
+function manageErrors($loc)
+    //Core files
+    
+    
+    if(!$loc) { //Try and figure out the problem.
+        
+    }
+?>
